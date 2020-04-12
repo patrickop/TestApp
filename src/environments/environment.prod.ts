146d2f99422817ec715e69f29152d42c625e2a6c
Patrick <patrick.opgenoorth@gmail.com>
2020-04-12 20:00:19 +0100

    add firebase creds

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyD5BgfMiO5v-JSDYr059QSFVPrm-6otsHE",
+    authDomain: "testapp-23cb5.firebaseapp.com",
+    databaseURL: "https://testapp-23cb5.firebaseio.com",
+    projectId: "testapp-23cb5",
+    storageBucket: "testapp-23cb5.appspot.com",
+    messagingSenderId: "170166160514",
+    appId: "1:170166160514:web:14b9755332bf99503df89b"
+}
+
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyD5BgfMiO5v-JSDYr059QSFVPrm-6otsHE",
+    authDomain: "testapp-23cb5.firebaseapp.com",
+    databaseURL: "https://testapp-23cb5.firebaseio.com",
+    projectId: "testapp-23cb5",
+    storageBucket: "testapp-23cb5.appspot.com",
+    messagingSenderId: "170166160514",
+    appId: "1:170166160514:web:14b9755332bf99503df89b"
+}
 };
 
 /*
